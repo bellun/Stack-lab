bbb38e2236ef062ca4efeed276a63829310751b2
Fernando H. Antero de Moraes <fhanteros@gmail.com>
2020-01-23 13:07:05 -0300

    Aula2
    Continuação aula 2 feita no trabalho

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express() 
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/',(request,response)=>{
+    response.render('home')
+})
+
+app.get('/vaga',(request,response)=>{
+    response.render('vaga')
+})
+
+app.listen(3000, (err)=>{
+    if(err){
+        console.log('Não foi possível iniciar o servidor do Jobfy')
+    }else{
+        console.log('Servidor do Jobfy rodando.')
+    }
+})
+//tempo 38:45
